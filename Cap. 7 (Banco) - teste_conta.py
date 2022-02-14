8c2ab4729510b1775a267681178a3e4575a92525
lzgandini <59964341+lzgandini@users.noreply.github.com>
2022-02-14 15:27:31 -0300

    Adicionado exercícios com o método histórico

--- Cap. 7 (Banco) - teste_conta.py
+++ Cap. 7 (Banco) - teste_conta.py
@@ -1,16 +1,22 @@
-from conta import Conta, Cliente
+from conta import Conta, Cliente, Historico
 
-cliente1 = Cliente('Ana', 'Calina', '111111111-11')
-cliente2 = Cliente('Marcela', 'Oliveira', '222222222-22')
+cliente1 = Cliente('Ana', 'Borges', '111111111-11', 'Ana Borges')
+cliente2 = Cliente('Marcela', 'Oliveira', '222222222-22', 'Marcela Oliveira')
 cliente3 = Cliente('Ana', 'Madeira', '333333333-33', 'Ana Madeira')
 
 conta1 = Conta('1', cliente1.nome_completo, 120.0)
 conta2 = Conta('2', cliente2.nome_completo, 300.0)
 conta3 = Conta('3', cliente3.nome_completo, 210.0)
 
-"""EXERCICIO N.12 (INCOMPLETO)
-Dúvida: Modifique o método extrato() da classe Conta para imprimir, além do número e saldo, os dados do cliente."""
+conta1.saca(30.0)
+conta2.transfere_para(conta3, 20.0)
+conta1.deposita(200.0)
+conta3.saca(300.0)
+conta3.transfere_para(conta1, 500.0)
 
-print('\nN. Conta: {}\nNome completo do titular: {}\nSaldo: {}'.format(conta1.numero, conta1.cliente, conta1.saldo))
-print('\nN. Conta: {}\nNome completo do titular: {}\nSaldo: {}'.format(conta2.numero, conta2.cliente, conta2.saldo))
-print('\nN. Conta: {}\nNome completo do titular: {}\nSaldo: {}'.format(conta3.numero, conta3.cliente, conta3.saldo))
+conta1.extrato()
+conta1.historico.imprime()
+conta2.extrato()
+conta2.historico.imprime()
+conta3.extrato()
+conta3.historico.imprime()
