31ecae6f28fba321e999cf3cfb437b3c034ed7e9
lzgandini <59964341+lzgandini@users.noreply.github.com>
2022-02-14 11:49:19 -0300

    Exercício n.12 (incompleto)
    Dúvida: Modifique o método extrato() da classe Conta para imprimir, além do número e saldo, os dados do cliente.

--- Cap. 7 (Banco) - conta.py
+++ Cap. 7 (Banco) - conta.py
@@ -12,18 +12,19 @@ class Historico:
             print('-', t)
 
 class Cliente:
-    def __init__(self, nome, sobrenome, cpf):
+    def __init__(self, nome, sobrenome, cpf, nome_completo):
         self.nome = nome
         self.sobrenome = sobrenome
         self.cpf = cpf
+        self.nome_completo = nome_completo
 
 class Conta:
     def __init__(self, numero, cliente, saldo, limite=1000.0):
         self.numero = numero
-        self.cliente = cliente
+        self.cliente = cliente  #agregação
         self.saldo = saldo
         self.limite = limite
-        self.historico = Historico()
+        self.historico = Historico()  #composição
 
     def deposita(self, valor):
         self.saldo += valor
@@ -36,14 +37,24 @@ class Conta:
             return True
 
     def transfere_para(self, destino, valor):
-        retirou = self.saca(valor)
-        if retirou == False:
-            return False
-        else:
+        if valor <= self.saldo:
+            self.saca(valor)
             destino.deposita(valor)
+            print('Transferência efetuada com sucesso!\n')
+
+            resposta = input('Deseja ver o seu saldo atual (sim/nao)? ')
+            if resposta == 'sim':
+                return self.extrato()
+            elif resposta == 'nao':
+                return 'Até mais!'
+            else:
+                return 'Resposta inválida'
+
+        else:
+            return 'Saldo insuficiente'
 
     def extrato(self):
-        print('Numero da conta: {}\nSaldo: R$ {}'.format(self.numero, self.saldo))
-        print('\nDADOS DO CLIENTE')
-        #print('Nome: {} {}'.format(Cliente.nome, Cliente.sobrenome))
+        return 'Numero da conta: {}\nSaldo: R$ {}'.format(self.numero, self.saldo)
+        #print('\nDADOS DO CLIENTE') #deve chamar outra classe
+        #print('Nome completo: {}'.format(Cliente.nome_completo))
         #print('CPF: {}'.format(Cliente.cpf))
